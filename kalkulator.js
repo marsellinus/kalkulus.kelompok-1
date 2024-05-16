2e8d711de6078ca5341a3a7bbf666180812494ee
marsellinus <45103898+marsellinus@users.noreply.github.com>
2024-05-16 21:07:19 +0700

    Update kalkulator.js
    Signed-off-by: marsellinus <45103898+marsellinus@users.noreply.github.com>

--- kalkulator.js
+++ kalkulator.js
@@ -1,44 +1,15 @@
 function generateSequence() {
     const sukuPertama = parseFloat(document.getElementById('sukuPertama').value);
     const beda = parseFloat(document.getElementById('Beda').value);
-    const sukuKeN = parseInt(document.getElementById('SukuKeN').value);
-    const choise = document.getElementById('choise').value;
+    const sukuKeN = parseFloat(document.getElementById('SukuKeN').value);
+    const jenis = document.getElementById('choise').value;
+    let hasil;
 
-    if (isNaN(sukuPertama) || isNaN(beda) || isNaN(sukuKeN) || sukuKeN <= 0) {
-        alert("Please enter valid numbers for all fields. Suku ke-n must be greater than 0.");
-        return;
+    if (jenis === 'baris') {
+        hasil = sukuPertama + (sukuKeN - 1) * beda;
+    } else if (jenis === 'deret') {
+        hasil = (sukuPertama + sukuPertama + (sukuKeN - 1) * beda) / 2 * sukuKeN;
     }
 
-    let result = "";
-    if (choise === "baris") {
-        result = calculateArithmeticSequence(sukuPertama, beda, sukuKeN);
-    } else if (choise === "deret") {
-        result = calculateArithmeticSeries(sukuPertama, beda, sukuKeN);
-    } else {
-        result = "Invalid choice.";
-    }
-
-    document.getElementById('sequenceResult').innerText = result;
+    document.getElementById('sequenceResult').innerHTML = `Nilai deret aritmatika adalah: ${hasil}`;
 }
-
-function calculateArithmeticSequence(a, d, n) {
-    let sequence = [];
-    for (let i = 0; i < n; i++) {
-        sequence.push(a + i * d);
-    }
-    return `The first ${n} terms of the arithmetic sequence are: ${sequence.join(", ")}`;
-}
-
-function calculateArithmeticSeries(a, d, n) {
-    let sum = (n / 2) * (2 * a + (n - 1) * d);
-    return `The sum of the first ${n} terms of the arithmetic series is: ${sum}`;
-}
-
-document.getElementById('menu-toggle').addEventListener('click', function() {
-            var menu = document.getElementById('mobile-menu');
-            if (menu.classList.contains('hidden')) {
-                menu.classList.remove('hidden');
-            } else {
-                menu.classList.add('hidden');
-            }
-        });
